b95c6d974f6a8b4f7abed253a2af3dddfcedf9e0
Raphael <raphael.bardot@gmail.com>
2018-06-12 18:51:59 -0400

    fix the problem to display message in file (no clean code)

--- app/UnwrappingMessageTest.cpp
+++ app/UnwrappingMessageTest.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdio>
 
+#include <inttypes.h>
 
 int main(){
     auto file = std::fopen("AWLMessages.txt", "w+");
@@ -11,9 +12,16 @@ int main(){
 
     for (auto i = 0; i < 10000; i++){
         AWLMessage message = kvaserCanProtocolStrategy->unwrapMessage();
-        auto test = message.messageData[0];
-//        auto messageData = std::string(reinterpret_cast<char*> (message.messageData), 8* message.messageLength);
-//        std::fprintf(file, "messageData:\t%x\t\n", test);
+        std::fprintf(file, "=================================================================================== \n");
+        std::fprintf(file, "ID : %" PRIu64 "\n",message.messageID);
+        std::fprintf(file, "Length : %" PRIu64 "\n",message.messageLength);
+        std::fprintf(file, "Timestamp : %" PRIu64 "\n",message.messageTimestamp);
+        for(int j=0 ; j<MESSAGE_DATA_LENGTH;j++) {
+            std::fprintf(file, "Data position %d (decimal): %d \n", j, message.messageData[j]);
+            if(j==MESSAGE_DATA_LENGTH-1){
+                std::fprintf(file, "Message en Hexadecimal %x %x %x %x %x %x %x %x \n",message.messageData[0],message.messageData[1],message.messageData[2],message.messageData[3],message.messageData[4],message.messageData[5],message.messageData[6],message.messageData[7]);
+            }
+        }
     }
 
     kvaserCanProtocolStrategy->closeConnection();
