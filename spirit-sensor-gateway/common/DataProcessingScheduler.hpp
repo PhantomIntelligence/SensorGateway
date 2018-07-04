6eddb8b12c28c06fb7ceaa12c2ce5b4228accf6c
Julie <julie.tetrault.2@gmail.com>
2018-07-04 14:39:23 -0400

    Change void function call

--- spirit-sensor-gateway/common/DataProcessingScheduler.hpp
+++ spirit-sensor-gateway/common/DataProcessingScheduler.hpp
@@ -32,7 +32,7 @@ namespace DataFlow {
                 terminateOrderReceived(false),
                 numberOfLinkedBuffers(0),
                 dataSink(dataSink),
-                schedulerThread(JoinableThread(voidAction())) {
+                schedulerThread(JoinableThread(voidAction)) {
             schedulerThread.exitSafely();
             schedulerThread = JoinableThread(&DataProcessingScheduler::start, this);
         }
