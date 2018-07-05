cc0475bc542ac33d6af6af9e306a91c1ff6c4753
Julie <julie.tetrault.2@gmail.com>
2018-07-05 14:03:09 -0400

    RRemove json and custom files generation in manual test

--- test/manual-test/kvaser-can-communication/UnwrappingMessageTest.cpp
+++ test/manual-test/kvaser-can-communication/UnwrappingMessageTest.cpp
@@ -9,8 +9,6 @@ int main(){
 
     const int NUMBER_DETECTION = 1000;
     auto file = std::fopen("AWLMessagesCustom.txt", "w+");
-    auto jsonFile = std::fopen("AWLMessages.json", "w+");
-    auto customFile = std::fopen("AWLMessagesCustom.txt","w+");
 
     KvaserCanProtocolStrategy kvaserCanProtocolStrategy;
     kvaserCanProtocolStrategy.openConnection();
@@ -33,64 +31,8 @@ int main(){
     }
 
 
-    //FOR GENERATING JSON FILE
-    std::fprintf(jsonFile, "[\n");
-    std::fprintf(jsonFile,"{ \"id\": 33, \"length\": 8, \"timestamp\": 3973015, \"data\": [4, 0, 64, 6, 0, 0, 117, 0]},\n");    for (auto i = 0; i < NUMBER_DETECTION; i++) {
-        AWLMessage message = kvaserCanProtocolStrategy.readMessage();
-        std::fprintf(jsonFile, "{");
-        std::fprintf(jsonFile, " \"id\": %" PRIu64 ",", message.id);
-        std::fprintf(jsonFile, " \"length\": %d,", message.length);
-        std::fprintf(jsonFile, " \"timestamp\": %" PRIu64 ",", message.timestamp);
-        std::fprintf(jsonFile, " \"data\": [");
-        for (int j = 0; j < message.length; j++) {
-            std::fprintf(jsonFile, "%d",message.data[j]);
-
-            if (j < message.length - 1) {
-                std::fprintf(jsonFile, ", ");
-            }
-        }
-        std::fprintf(jsonFile, "]");
-        if (i < NUMBER_DETECTION - 1) {
-            std::fprintf(jsonFile, "}, \n");
-            if(i == NUMBER_DETECTION/2){
-                std::fprintf(jsonFile,"{ \"id\": 27, \"length\": 8, \"timestamp\": 3973015, \"data\": [4, 0, 64, 6, 0, 0, 117, 0]},\n");
-            }
-        }
-        else{
-            std::fprintf(jsonFile, "} \n");
-        }
-    }
-    std::fprintf(jsonFile, "]");
-
-    //FOR GENERATING CUSTOM FILE
-    std::fprintf(customFile,"33-8-3973015-4-0-64-6-0-0-117-0\n");
-    for (auto i = 0; i < NUMBER_DETECTION; i++) {
-        AWLMessage message = kvaserCanProtocolStrategy.readMessage();
-        std::fprintf(customFile, "%" PRIu64"-", message.id);
-        std::fprintf(customFile, "%d-", message.length);
-        std::fprintf(customFile, "%" PRIu64"-", message.timestamp);
-        for (int j = 0; j < message.length; j++) {
-            if (j < message.length - 1) {
-                std::fprintf(customFile,"%d-",message.data[j]);
-            }
-            else{
-                std::fprintf(customFile, "%d",message.data[j]);
-            }
-        }
-        if(i == NUMBER_DETECTION/2){
-            std::fprintf(customFile,"\n27-8-3973015-4-0-64-6-0-0-117-0\n");
-        }
-        else{
-            std::fprintf(customFile, "\n");
-        }
-    }
-
     kvaserCanProtocolStrategy.closeConnection();
     fflush(file);
     fclose(file);
-    fflush(jsonFile);
-    fclose(jsonFile);
-    fflush(customFile);
-    fclose(customFile);
     return 0;
 };
