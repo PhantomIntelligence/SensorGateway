427ce599d244a9d037c57cfa304a9e146f17f194
sambouchard <samuel.bouchard.7@ulaval.ca>
2018-07-17 15:45:18 -0400

    Finish File-In/File-Out test

--- test/test-suite/server-communication/ParseFrameToJsonStringTest.cpp
+++ test/test-suite/server-communication/ParseFrameToJsonStringTest.cpp
@@ -2,13 +2,21 @@
 #define SPIRITSENSORGATEWAY_PARSEFRAMETOJSONSTRINGTEST_CPP
 
 #include <gtest/gtest.h>
+#include <fstream>
+#include <spirit-sensor-gateway/domain/AWLMessage.h>
+#include <test/utilities/files/AWLMessagesFileManager.h>
 #include "spirit-sensor-gateway/server-communication/JsonParser.h"
+#include "test/utilities/files/FileManager.hpp"
 
 using DataFlow::Frame;
+using DataFlow::AWLMessage;
 using ServerCommunication::JsonParser;
+using TestUtilities::AWLMessagesFileManager;
+using DataFlow::PixelsArray;
+using DataFlow::TracksArray;
+using Defaults::Pixel::DEFAULT_TRACKS_ARRAY;
 using Defaults::Frame::DEFAULT_FRAME;
-using Defaults::Pixel::DEFAULT_PIXEL;
-using Defaults::Track::DEFAULT_TRACK;
+using Defaults::Frame::DEFAULT_PIXELS_ARRAY;
 
 
 class ParseFrameToJsonStringTest : public ::testing::Test {
@@ -28,10 +36,26 @@ TEST_F(ParseFrameToJsonStringTest,
 }
 
 TEST_F(ParseFrameToJsonStringTest, given_aFrame_when_parsongItToAJsonString_then_returnTheCorrectlyParsedJSonString) {
-    Frame testFrame = DEFAULT_FRAME;
+    TracksArray tracksArray = DEFAULT_TRACKS_ARRAY;
+    tracksArray[2].ID =44;
+    tracksArray[2].intensity =44;
+    tracksArray[2].speed =44;
+    tracksArray[2].confidenceLevel =44;
+    tracksArray[2].distance =44;
+    tracksArray[2].acceleration =44;
+    Pixel pixel = Pixel(4,tracksArray,0);
+    PixelsArray pixelsArray = DEFAULT_PIXELS_ARRAY;
+    pixelsArray[1] = pixel;
+    Frame testFrame = Frame(48, 47, pixelsArray);
 
     std::string string = JsonParser::parseFrameToJsonString(testFrame);
-    std::cout<<string<<std::endl;
+    std::ofstream file;
+    file.open("ActualFrame.json");
+    file << string << std::endl;
+    file.close();
+    AWLMessagesFileManager fileManager;
+    bool areEqual = fileManager.areFilesEqual("ActualFrame.json", "ExpectedFrame.json");
+    ASSERT_TRUE(areEqual);
 
 }
 
