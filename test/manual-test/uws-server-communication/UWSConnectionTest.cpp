66eca3f0d03efa81b3a6c7c0f55e2f3491b7320d
sambouchard <samuel.bouchard.7@ulaval.ca>
2018-07-18 16:47:41 -0400

    Merge branch 'dev/SPIR-471/implemention-with-uws' into feature/SPIR-487/implement-object-to-JSON-transformer-for-communication
    # Conflicts:
    #	spirit-sensor-gateway/CMakeLists.txt
    #	spirit-sensor-gateway/domain/Pixel.cpp
    #	test/CMakeLists.txt

--- test/manual-test/uws-server-communication/UWSConnectionTest.cpp
+++ test/manual-test/uws-server-communication/UWSConnectionTest.cpp
@@ -19,8 +19,11 @@
 
 #include "spirit-sensor-gateway/server-communication/UWSServerCommunicationStrategy.h"
 #include "test/utilities/data-model/DataModelFixture.h"
+#include "test/utilities/stub/SpiritFramesStub.h"
 
 namespace ManualTest {
+    using Stub::createArbitrarySpiritFrame;
+    using ServerCommunication::UWSServerCommunicationStrategy;
 
     void coutMessage(std::string const& message) {
         std::cout << message << std::endl;
@@ -101,7 +104,8 @@ namespace ManualTest {
                     ws->close(1000);
                     break;
                 default:
-                    std::cout << "FAILURE: " << ws->getUserData() << " should not setWebSocketCallbacksAndConnection!" << std::endl;
+                    std::cout << "FAILURE: " << ws->getUserData() << " should not setWebSocketCallbacksAndConnection!"
+                              << std::endl;
                     exit(-1);
             }
         });
@@ -198,15 +202,12 @@ namespace ManualTest {
         uWS::Hub hub;
 
 
-
         WebSocketPointerPromise webSocketPointerPromise;
 
         auto webSocketConnectionThread = JoinableThread(&connectAndStartWebSocket, &hub, &webSocketPointerPromise);
         auto futureWebSocket = webSocketPointerPromise.get_future();
         futureWebSocket.wait();
         auto webSocket = futureWebSocket.get();
-
-
 
 
         auto numberOfSentMessages = 100;
@@ -225,15 +226,24 @@ namespace ManualTest {
         return 0;
     }
 
+    void testConnectionWithConcreteStrategy() {
+        UWSServerCommunicationStrategy uwsServerCommunicationStrategy;
+        uwsServerCommunicationStrategy.openConnection();
+        auto frame = createArbitrarySpiritFrame();
+        uwsServerCommunicationStrategy.sendMessage(std::move(frame));
+
+    }
+
 
 }
 
 int main() {
 //    auto testOutput = ManualTest::testBasicUWSConnectionCalls();
-    auto testOutput = ManualTest::testMessageSending();
-    if (testOutput == 0) {
-
-    }
+//    auto testOutput = ManualTest::testMessageSending();
+//    if (testOutput == 0) {
+//
+//    }
+    ManualTest::testConnectionWithConcreteStrategy();
     return 0;
 }
 
